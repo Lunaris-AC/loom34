eb7594d9dae5a5e6d0f7e53e32a4b8b13d3a75b5
Axel Calcavecchia <82838999+Lunaris-AC@users.noreply.github.com>
2025-04-10 17:02:22 +0200

    fixing broken links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,11 +39,11 @@ const Navbar = () => {
     { name: 'Home', path: '/' },
     { name: 'About', path: '/about' },
     { name: 'Events', path: '/events' },
-    { name: 'Shop', url: 'https://www.helloasso.com/associations/association-les-ours-occitanie-mediterranee-loom#shop', external: true },
+    { name: 'Shop', path: '/shop', external: true },
     { name: 'Monsieur Ours', path: '/monsieur-ours' },
     { name: 'Gallery', path: '/gallery' },
     { name: 'Partners', path: '/partners' },
-    { name: 'Membership', url: 'https://www.helloasso.com/associations/association-les-ours-occitanie-mediterranee-loom/adhesions/adhesion-2024-2025', external: true },
+    { name: 'Membership', path: '/membership', external: true },
   ];
   
   const isActive = (path: string) => {
